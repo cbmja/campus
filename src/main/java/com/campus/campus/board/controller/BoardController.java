07a8fad4ac7bec29a55269ca16d654a753fe0b49
cbmja <cbmja@naver.com>
2024-04-22 12:47:09 +0900

    4/22 게시판 글 작성 ok

--- src/main/java/com/campus/campus/board/controller/BoardController.java
+++ src/main/java/com/campus/campus/board/controller/BoardController.java
@@ -1,12 +1,13 @@
 package com.campus.campus.board.controller;
 
+import com.campus.campus.board.entity.BoardData;
+import com.campus.campus.board.service.BoardDataInfoService;
+import com.campus.campus.board.service.BoardDataSaveService;
 import com.campus.campus.config.MenuConfig;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/board")
@@ -15,14 +16,38 @@ public class BoardController {
 
     private final MenuConfig menuConfig;
 
+    private final BoardDataSaveService boardDataSaveService;
+    private final BoardDataInfoService boardDataInfoService;
+
 
     @GetMapping("/list")
-    public String dataList(Model model , @RequestParam(name = "code") String code){
+    public String list(Model model , @RequestParam(name = "code") String code){
+        model.addAttribute("menus" , menuConfig.getBoardMenu());
+        model.addAttribute("code" , code);
+        model.addAttribute("boardDataList" , boardDataInfoService.getList(code));
+
+
+        return "board/list";
+    }
+
+
+    @GetMapping("/create")
+    public String create(Model model , @RequestParam(name = "code" , required = false) String code){
         model.addAttribute("menus" , menuConfig.getBoardMenu());
         model.addAttribute("code" , code);
 
+        return "board/upload";
+    }
+
+    @PostMapping("/create")
+    public String createProc(Model model , @ModelAttribute BoardData boardData){
+        model.addAttribute("menus" , menuConfig.getBoardMenu());
+        model.addAttribute("code" , boardData.getCode());
+        System.out.println(boardData);
 
 
+        boardDataSaveService.save(boardData);
+        model.addAttribute("boardDataList" , boardDataInfoService.getList(boardData.getCode()));
 
         return "board/list";
     }
